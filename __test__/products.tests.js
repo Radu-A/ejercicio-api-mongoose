e8d37ea53d70385b0d5266ca4ac8b4e78c8a466b
Radu-A <elcastravete@gmail.com>
2023-09-26 12:03:52 +0200

    redirect/swagger_home

---
+++ __test__/products.tests.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET / all products', () => {
+    it('Respond with json containing a list of all products', done => {
+        
+    })
+})
--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-const providersRouter = require('./routes/providersRoutes');
-const productsRouter = require('./routes/productsRoutes');
+const providersRouter = require("./routes/providersRoutes");
+const productsRouter = require("./routes/productsRoutes");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.use(express.static('public'));
-app.get('/', async (req, res) => {
-    res.render('home');
-})
-
-app.use('/api/providers', providersRouter);
-app.use('/api/products', productsRouter);
+app.use("/api/providers", providersRouter);
+app.use("/api/products", productsRouter);
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-})
+  console.log(`http://localhost:${port}`);
+});
+
+module.exports = app;
